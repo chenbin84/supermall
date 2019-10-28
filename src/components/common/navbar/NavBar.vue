cb9eade9ef7451634434cf486112423883e222f9
chenbin <649835367@qq.com>
2019-10-28 16:09:32 +0800

    首页

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="nav-bar">
+      <div class="left"><slot name="left"></slot></div>
+      <div class="center"><slot name="center"></slot></div>
+      <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped >
+    .nav-bar {
+        display: flex;
+        height: 44px;
+        line-height: 44px;
+        text-align:center;
+        box-shadow: 0 1px 1px  rgba(100, 100, 100, .1)
+    }
+    .left,.right {
+        width: 60px;
+        /* background-color: red */
+    } 
+    .center {
+        /* 占据其他部分 */
+        flex:1;
+        /* background-color: blue */
+    }
+</style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,35 @@
+<template>
+  <swiper>
+        <swiper-item  v-for="item in banners">
+          <a :href="item.link">
+            <img :src="item.image" alt="">
+          </a>
+        </swiper-item>
+      </swiper>
+</template>
+
+<script>
+// import {getHomeMultidata} from 'network/home'
+import {Swiper,SwiperItem} from 'components/common/swiper'
+export default {
+  data() {
+    return {
+
+    }
+  },
+  props:{
+      banners:Array,
+      default() {
+          return []
+      }
+  },
+  components: {
+      Swiper,
+      SwiperItem
+  }
+}
+</script>
+
+<style scoped >
+
+</style>
